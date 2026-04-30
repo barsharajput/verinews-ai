65e46884fe653809d9206f47c4248a32bf2c50d0
Barsha Singh <barsharajput22@gmail.com>
2026-04-30 21:22:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -385,12 +385,8 @@ def clear_history():
     db.session.commit()
     return redirect(url_for("history"))
 
+with app.app_context():
+    db.create_all()
 
-# ==============================
-# 🚀 RUN APP
-# ==============================
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
